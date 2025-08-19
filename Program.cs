c739f91caf5ecb4ba3c6cf6b307567b3dd547cd4
MariPinas <marianapls2003@gmail.com>
2025-08-19 13:52:18 -0300

    :sparkles: Desafio finalizado!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "111243", modelo: "modelo 1",imei: "11111111", memoria: 8);
+Console.WriteLine("\n**** Nokia ****");
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+nokia.ReceberLigacao();
+
+Console.WriteLine("\n**** Iphone ****");
+Smartphone iphone = new Iphone(numero: "224124", modelo: "modelo 2",imei: "2222222", memoria: 16);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+iphone.ReceberLigacao();
